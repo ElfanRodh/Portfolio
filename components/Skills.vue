17a537d6041ab932788e9cb3d958d14f9524b487
Elfan Rodhian <elfanapoywali@gmail.com>
2022-01-31 16:57:36 +0700

    update

--- components/Skills.vue
+++ components/Skills.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="flex flex-wrap items-center justify-center md:justify-center font-semibold text-white text-center pt-6 md:p-8">
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://assets.website-files.com/5d8a2888296e91abbdcb65f0/5dc23b7673f2302766ce6534_HTML%205.png" alt="HTML 5" class="w-12 h-12 m-auto rounded-xl active:scale-95"> 
+          <img src="~assets/skills/html.png" alt="HTML 5" class="w-12 h-12 m-auto p-1 rounded-xl active:scale-95"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">HTML</div>
           <!-- <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-2 text-center">
             <div class="bg-sky-600 h-2.5 rounded-full" style="width: 95%">
@@ -17,7 +17,7 @@
           </div> -->
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://assets.website-files.com/5d8a2888296e91abbdcb65f0/5dba8872b9f0e7853becd711_1yWl7jnivvO_7Xc3fy4gYMGNFvVAR2mIv7mN7DJhcfA.png" alt="CSS" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/css.png" alt="CSS" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">CSS</div>
           <!-- <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-2 text-center">
             <div class="bg-sky-600 h-2.5 rounded-full" style="width: 95%">
@@ -26,43 +26,43 @@
           </div> -->
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://assets.website-files.com/5d8a2888296e91abbdcb65f0/5dba888dd4c5987ae9a9a418_1orUr-b5kcN84R0LHzF80QcD1YQTA83KFGb_bN_p9ow.png" alt="Javascript" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/js.png" alt="Javascript" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Javascript</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://download.logo.wine/logo/MySQL/MySQL-Logo.wine.png" alt="MySQL" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/mysql.png" alt="MySQL" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">MySQL</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://cdn.iconscout.com/icon/free/png-256/postgresql-226047.png" alt="Postgress" class="w-12 h-12 m-auto rounded-xl"> 
-          <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Postgress</div>
+          <img src="~assets/skills/postgress.png" alt="Postgress" class="w-12 h-12 m-auto p-1 rounded-xl"> 
+          <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">PostgresSQL</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://rexavllp.com/assets/img/codeigniter.png" alt="Codeigniter" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/codeigniter.png" alt="Codeigniter" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">CodeIgniter</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://assets.website-files.com/5d8a2888296e91abbdcb65f0/5ddb33ab967a3bda6d371a71_laravel.png" alt="Laravel" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/laravel.png" alt="Laravel" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Laravel</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJyUEdgRMkgGHbaXMK2A0Kt5FiShIMV1xvRF8DVa90FKYNe6GAGqcb9E4tgqHw1tTpCuc&usqp=CAU" alt="Flutter" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/flutter.png" alt="Flutter" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Flutter</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://docs.vuejs.id//images/logo.png" alt="Vue" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/vue.png" alt="Vue" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Vue</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://cdn.iconscout.com/icon/free/png-256/bootstrap-226077.png" alt="Bootstrap" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/bootstrap.png" alt="Bootstrap" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Bootstrap</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://avatars0.githubusercontent.com/u/67109815?v=4" alt="Tailwind" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/tailwind.png" alt="Tailwind" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Tailwind</div>
         </div>
         <div class="p-2 m-4 bg-blue-50 shadow-lg shadow-neo-light active:shadow-neo-light-focus dark:shadow-neo-dark dark:active:shadow-neo-dark-focus dark:bg-gray-900 rounded-2xl transition duration-200 hover:scale-110 w-32">
-          <img src="https://i.pinimg.com/originals/17/06/c9/1706c9f16bd08eb5e03f1df3e0a94a1c.png" alt="Figma" class="w-12 h-12 m-auto rounded-xl"> 
+          <img src="~assets/skills/figma.png" alt="Figma" class="w-12 h-12 m-auto p-1 rounded-xl"> 
           <div class="pt-4 text-xs md:text-sm text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-400">Figma</div>
         </div>
       </div>
